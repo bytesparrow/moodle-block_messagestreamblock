d66daa1904bebba221ad35ccfdc09d49bbceca26
bytesparrow <5909549+bytesparrow@users.noreply.github.com>
2025-08-07 15:25:03 +0200

    version update

--- version.php
+++ version.php
@@ -8,7 +8,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_messagestreamblock';
-$plugin->version = 2025080603;
+$plugin->version = 2025080604;
 $plugin->requires = 2024052700; // Moodle 4.5
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = 'v0.1';
