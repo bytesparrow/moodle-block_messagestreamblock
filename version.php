3fff1205eb4003592340a004d6c7816b815f6540
bytesparrow <5909549+bytesparrow@users.noreply.github.com>
2025-08-14 14:30:56 +0200

    Some design changes especially for mobile

--- version.php
+++ version.php
@@ -8,7 +8,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_messagestreamblock';
-$plugin->version = 2025080608;
+$plugin->version = 2025080609;
 $plugin->requires = 2024052700; // Moodle 4.5
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = 'v0.1';
